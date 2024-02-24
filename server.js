8c89a4641b7f499dbc0a29b60cc5ae9d6e554379
Jay Termulo <jetermulo@gmail.com>
2024-02-24 11:12:26 +0800

    debug server error

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(
+app.use('*',
   cors({
-    origin: ["http://localhost:5173", "https://pinvent-app.vercel.app"],
+    origin: ["http://localhost:5173", "https://leave-management-app-client.vercel.app/"],
     credentials: true,
   })
 );
